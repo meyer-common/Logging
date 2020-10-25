229fbe084f1f3628c3786f3e873c9c61e61b8e67
ameyer15801 <54753336+ameyer15801@users.noreply.github.com>
2020-10-25 03:01:55 -0500

    init

---
+++ Logging/Logging/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Meyer.Common.Logging
+{
+    public class LogEntry
+    {
+        public DateTimeOffset TimeStamp { get; set; }
+
+        public string Level { get; set; }
+
+        public string Class { get; set; }
+
+        public string Namespace { get; set; }
+
+        public string Message { get; set; }
+
+        public string StackTrace { get; set; }
+    }
+}
---
+++ Logging/Logging/LogEntryJsonFormatter.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Serilog.Events;
+using Serilog.Formatting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Meyer.Common.Logging
+{
+    public class IdmCompactJsonFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            var exception = logEvent.Exception;
+
+            logEvent.Properties.TryGetValue("contextType", out LogEventPropertyValue leClass);
+            logEvent.Properties.TryGetValue("SourceContext", out LogEventPropertyValue leSourceContext);
+
+            List<LogEntry> listOfLogEntries = new List<LogEntry>();
+            do
+            {
+                listOfLogEntries.Add(new LogEntry
+                {
+                    Message = exception != null ? exception.Message : logEvent.MessageTemplate.Text,
+                    Level = logEvent.Level.ToString().Trim('"'),
+                    TimeStamp = logEvent.Timestamp,
+                    StackTrace = exception?.StackTrace,
+                    Namespace = leSourceContext?.ToString().Trim('"'),
+                    Class = leClass?.ToString().Trim('"')
+                });
+
+                exception = exception?.InnerException;
+            }
+            while (exception != null);
+
+            output.Write(JsonConvert.SerializeObject(listOfLogEntries));
+        }
+    }
+}
